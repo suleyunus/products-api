f18b4476a89fe88bb3fa816f96884d62fd997ba0
suleyunus <suleimanyunus778@gmail.com>
2019-10-18 07:36:07 +0300

    feature(admin): register products model to admin site

--- products/models.py
+++ products/models.py
@@ -4,3 +4,10 @@ class Products(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
