f74bf738e5e84cad1c261b39b65c889af7a073f6
suleyunus <suleimanyunus778@gmail.com>
2019-10-18 08:24:51 +0300

    feature(auth): add token authentication

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
     'products',
 ]
@@ -122,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import (
     ProductsView
 )
 
 urlpatterns = [
+    path('authtoken/', obtain_auth_token),
     path('products/', ProductsView.as_view()),
 ]
